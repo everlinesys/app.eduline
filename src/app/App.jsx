6eba876aa669c523f9b25f8f998e29721fa2e3fa
Azhar <azhar.subair.2@gmail.com>
2026-04-12 21:55:18 +0530

    first commit

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,8 +4,13 @@ import { useEffect, useState } from "react";
 import OfflineScreen from "../public/pages/OfflineScreen";
 
 export default function App() {
-   const [isOnline, setIsOnline] = useState(navigator.onLine);
-
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+  document.addEventListener("dblclick", (e) => {
+    e.preventDefault();
+  }, { passive: false });
+  document.addEventListener("gesturestart", (e) => {
+    e.preventDefault();
+  });
   useEffect(() => {
     const goOnline = () => setIsOnline(true);
     const goOffline = () => setIsOnline(false);
